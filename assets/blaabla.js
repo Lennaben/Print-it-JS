0d44d0f8d059471c120c06024d7826b351cea5d1
Lennaben <BENALOUILENNA@GMAIL.COM>
2023-02-13 14:41:12 +0100

    indentation

--- assets/blaabla.js
+++ assets/blaabla.js
@@ -1,4 +1,8 @@
-let countSlide = 0;
+let countSlide = 0
+
+//let change, const non
+
+// les [] = tableau, le tableau des slides
 
 const slides = [
   {
@@ -18,69 +22,74 @@ const slides = [
     image: "slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
-];
+]
 
-const leftArrow = document.querySelector(".arrow_left");
-const rightArrow = document.querySelector(".arrow_right");
+// document = chercher dans toute la page, querySelector = qui remonte l'element HTML qui as applé la class
 
-let allDots = [];
+const leftArrow = document.querySelector(".arrow_left")
+const rightArrow = document.querySelector(".arrow_right")
+
+let allDots = []
+
+// addEventListener = rajoute un "ecouteur" = quand on clik sur leftArrow
+// ()
 
 leftArrow.addEventListener("click", (e) => {
-  e.preventDefault();
+  e.preventDefault()
   if (countSlide <= slides.length && countSlide > 0) {
-    countSlide--;
+    countSlide--
   } else {
-    countSlide = 4;
+    countSlide = 4
   }
+
   //   console.log("clik droit", countSlide);
-  const dots = document.querySelectorAll(".dot");
+  const dots = document.querySelectorAll(".dot")
   for (const dot of dots) {
-    dot.classList.remove("dot_selected");
+    dot.classList.remove("dot_selected")
   }
-  document.querySelectorAll(".dot")[countSlide].classList.add("dot_selected");
-  console.log(countDot);
-  let img = document.querySelector(".banner-img");
-  img.src = "./assets/images/slideshow/" + slides[countSlide].image;
-  let tagPar = document.querySelector(".tagPar");
-  tagPar.innerHTML = slides[countSlide].tagLine;
-});
+  document.querySelectorAll(".dot")[countSlide].classList.add("dot_selected")
+  console.log(countDot)
+  let img = document.querySelector(".banner-img")
+  img.src = "./assets/images/slideshow/" + slides[countSlide].image
+  let tagPar = document.querySelector(".tagPar")
+  tagPar.innerHTML = slides[countSlide].tagLine
+})
 
 rightArrow.addEventListener("click", (e) => {
-  e.preventDefault();
+  e.preventDefault()
   if (countSlide >= slides.length) {
-    countSlide = 0;
+    countSlide = 0
   } else {
-    countSlide++;
+    countSlide++
   }
   //   console.log("clik droit", countSlide);
-  const dots = document.querySelectorAll(".dot");
+  const dots = document.querySelectorAll(".dot")
   for (const dot of dots) {
-    dot.classList.remove("dot_selected");
+    dot.classList.remove("dot_selected")
   }
-  document.querySelectorAll(".dot")[countSlide].classList.add("dot_selected");
-  console.log(countDot);
-  let img = document.querySelector(".banner-img");
-  img.src = "./assets/images/slideshow/" + slides[countSlide].image;
-  let tagPar = document.querySelector(".tagPar");
+
+  document.querySelectorAll(".dot")[countSlide].classList.add("dot_selected")
+  
+  console.log(countDot)
+  let img = document.querySelector(".banner-img")
+  img.src = "./assets/images/slideshow/" + slides[countSlide].image
+  let tagPar = document.querySelector(".tagPar")
   tagPar.innerHTML = slides[countSlide].tagLine
-});
+})
 
-const containerPoints = document.querySelector(".dots");
+const containerPoints = document.querySelector(".dots")
 
-let countDot = 0;
+let countDot = 0
 slides.map((slide, i) => {
-  console.log(i);
-  const point = document.createElement("div");
-  point.classList.add("dot");
+  console.log(i)
+  const point = document.createElement("div")
+  point.classList.add("dot")
 
   if (countDot === 0) {
-    point.classList.add("dot_selected");
-    countDot++;
+    point.classList.add("dot_selected")
+    countDot++
   }
 
-  
-
-  containerPoints.appendChild(point);
-
-  allDots.push(point);
-});
+  containerPoints.appendChild(point)
+  allDots.push(point)
+})
